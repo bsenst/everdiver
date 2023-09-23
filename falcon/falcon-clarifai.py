91aa3c5fc35f9dad1d0239c8a29aac3785a47e41
bsenst <benjamin.senst@gmail.com>
2023-09-23 14:00:05 +0200

    add clarifai falcon

---
+++ falcon/falcon-clarifai.py
@@ -0,0 +1,73 @@
+
+######################################################################################################
+# In this section, we set the user authentication, user and app ID, model details, and the URL of 
+# the text we want as an input. Change these strings to run your own example.
+######################################################################################################
+
+# Your PAT (Personal Access Token) can be found in the portal under Authentification
+PAT = input("Enter your Clarifai personal access token")
+# Specify the correct user_id/app_id pairings
+# Since you're making inferences outside your app's scope
+USER_ID = 'tiiuae'
+APP_ID = 'falcon'
+# Change these to whatever model and text URL you want to use
+MODEL_ID = 'falcon-40b-instruct'
+MODEL_VERSION_ID = '1f704e8d43d949348fbf5f9b8cecfca8'
+RAW_TEXT = 'I love your product very much'
+# To use a hosted text file, assign the url variable
+# TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
+# Or, to use a local text file, assign the url variable
+# TEXT_FILE_LOCATION = 'YOUR_TEXT_FILE_LOCATION_HERE'
+
+############################################################################
+# YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+############################################################################
+
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
+channel = ClarifaiChannel.get_grpc_channel()
+stub = service_pb2_grpc.V2Stub(channel)
+
+metadata = (('authorization', 'Key ' + PAT),)
+
+userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+
+# To use a local text file, uncomment the following lines
+# with open(TEXT_FILE_LOCATION, "rb") as f:
+#    file_bytes = f.read()
+
+post_model_outputs_response = stub.PostModelOutputs(
+    service_pb2.PostModelOutputsRequest(
+        user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
+        model_id=MODEL_ID,
+        version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
+        inputs=[
+            resources_pb2.Input(
+                data=resources_pb2.Data(
+                    text=resources_pb2.Text(
+                        raw=RAW_TEXT
+                        # url=TEXT_FILE_URL
+                        # raw=file_bytes
+                    )
+                )
+            )
+        ]
+    ),
+    metadata=metadata
+)
+if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+    print(post_model_outputs_response.status)
+    raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+
+# Since we have one input, one output will exist here
+output = post_model_outputs_response.outputs[0]
+
+print("Predicted concepts:")
+for concept in output.data.concepts:
+    print("%s %.2f" % (concept.name, concept.value))
+
+# Uncomment this line to print the full Response JSON
+print(output)
+
---
+++ streamlit/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Welcome")
+
+st.write("this app is under construction")
